5deba3fc60970b27cde85fd5c4b4a588c31ee678
jenny <jenny.jione.jang@gmail.com>
2021-10-05 11:32:29 +0900

    modify ejs name and apply it to app.js

--- Web/app.js
+++ Web/app.js
@@ -14,19 +14,19 @@ var data = {count:0};
 
 app.get('/', function(req, res){
   data.count++;
-  res.render('homepage', data);
+  res.render('mainpage', data);
 });
 app.get('/reset', function(req, res){
   data.count=0;
-  res.render('homepage', data);
+  res.render('mainpage', data);
 });
 app.get('/set/count', function(req, res){
   if(req.query.count) data.count=req.query.count;
-  res.render('homepage', data);
+  res.render('mainpage', data);
 });
 app.get('/set/:num', function(req, res){
   data.count = req.params.num;
-  res.render('homepage', data);
+  res.render('mainpage', data);
 });
 
 
