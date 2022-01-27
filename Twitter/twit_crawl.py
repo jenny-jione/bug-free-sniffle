0e126cc840721b5479d4f07fbc5100b6c1a79dcc
jenny <jenny.jione.jang@gmail.com>
2022-01-27 12:53:46 +0900

    Add code; save crawling data to csv file

--- Twitter/twit_crawl.py
+++ Twitter/twit_crawl.py
@@ -1,6 +1,7 @@
 import twitter
 from my_info import *
 import datetime
+import csv
 
 
 def changeDateFormat(str_dt):
@@ -9,6 +10,17 @@ def changeDateFormat(str_dt):
     kst = dt_dt + datetime.timedelta(hours=9)
     format2 = '%Y.%m.%d  %p %I:%M'
     return datetime.datetime.strftime(kst, format2)
+
+
+def save_to_file(twts):
+    print("save to file")
+    file = open(my_path + f"/Twitter/BTS_twt.csv",
+                mode="w", newline="", encoding='utf-8-sig')
+    writer = csv.writer(file)
+    writer.writerow(["date", "text", "pic"])
+    for twt in twts:
+        writer.writerow(twt)
+    return True
 
 
 api = twitter.Api(consumer_key=twitter_consumer_key,
@@ -21,17 +33,36 @@ result = api.GetUserTimeline(screen_name=account, count=200, include_rts=False, 
 print(len(result))
 
 
-for twt in result:
-    print(changeDateFormat(twt.created_at))
-    print(twt.text)
-    if(twt.media):
-        # print(twt.media[0])
-        # print(twt.media[0].media_url_https)
-        print(len(twt.media))
-        pic_num = len(twt.media)
-        for i in range(pic_num):
-            print(twt.media[i].media_url_https)
-    print()
-    print()
+# for twt in result:
+#     print(changeDateFormat(twt.created_at))
+#     print(twt.text)
+#     if(twt.media):
+#         # print(twt.media[0])
+#         # print(twt.media[0].media_url_https)
+#         print(len(twt.media))
+#         pic_num = len(twt.media)
+#         for i in range(pic_num):
+#             print(twt.media[i].media_url_https)
+#     print()
+#     print()
 
-print(result[7])
+twts = []
+cnt = 1
+for r in result:
+    twt = []
+    twt.append(changeDateFormat(r.created_at))
+    twt.append(r.text)
+    if(r.media):
+        pic_num = len(r.media)
+        for i in range(pic_num):
+            pics = []
+            pics.append(r.media[i].media_url_https)
+        twt.append(pics)
+    else:
+        twt.append(None)
+    twts.append(twt)
+    cnt += 1
+    print(cnt)
+
+print(twts)
+save_to_file(twts)
