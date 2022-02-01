bb447348ec6ae660773ca571021d6537f2d72b25
jenny <jenny.jione.jang@gmail.com>
2022-02-01 20:58:10 +0900

    FEAT: try-except문, get_monthly_top_songs(), get_monthly_1st_song()

--- Selenium/melon_selenium.py
+++ Selenium/melon_selenium.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import my_info
 import time
+import csv
 
 driver = webdriver.Chrome(my_info.my_path+'/Selenium/chromedriver.exe')
 url = 'https://member.melon.com/muid/web/login/login_informM.htm'
@@ -19,38 +20,97 @@ driver.get(url_mypage+my_info.memberKey)
 # time.sleep(2)
 
 
-# 선택한 월의 곡 목록 가져오기
-def get_monthly_song_list():
-    monthly_song_list = []
-    print("calling function")
-    time.sleep(1)
+def save_to_file(lists):
+    print("save to file")
+    file = open(my_info.my_path + f"/Selenium/Monthly_1st_song_2016_2021.csv",
+                mode="a", newline="", encoding='utf-8-sig')
+    writer = csv.writer(file)
+    writer.writerow(["year", "month", "title"])
+    for list in lists:
+        writer.writerow(list)
+    return True
+
+# 해당 월의 1st 노래만 가져오기
+def get_monthly_1st_song(month, year):
+    monthly_1st_songs = []
+    time.sleep(0.1)
+    try:
+        monthly_1st_song = []
+        tbody = driver.find_element(By.TAG_NAME, "tbody")
+        trs = tbody.find_elements(By.TAG_NAME, "tr")
+        tleft = trs[0].find_element(By.CLASS_NAME, "t_left")
+        title = tleft.find_elements(By.TAG_NAME, "a")[1].text
+        print(title)
+        print()
+        monthly_1st_song.append(year)
+        monthly_1st_song.append(month)
+        monthly_1st_song.append(title)
+
+        monthly_1st_songs.append(monthly_1st_song)
+    except:
+        print("해당 월에 들은 노래가 없습니다.")
+        print()
+        monthly_1st_songs.append("None")
+    return monthly_1st_songs
+
+
+
+# 선택한 월의 TOP 10 목록 가져오기
+def get_monthly_top_songs(month, year, num):
+    monthly_top_songs = []
+    time.sleep(0.5)
     tbody = driver.find_element(By.TAG_NAME, "tbody")
     trs = tbody.find_elements(By.TAG_NAME, "tr")
-    for tr in trs:
-        msl = []
-        rank = tr.find_element(By.CLASS_NAME, "no").text
-        tleft = tr.find_element(By.CLASS_NAME, "t_left")
-        title = tleft.find_elements(By.TAG_NAME, "a")[1].text
-        artist = tr.find_element(By.ID, "artistName").text
-        msl.append(rank)
-        msl.append(title)
-        msl.append(artist)
-        monthly_song_list.append(msl)
-    print(monthly_song_list)
-    return monthly_song_list
+    print(len(trs))
+
+    for i in range(num):
+        try:
+            msl = []
+            rank = trs[i].find_element(By.CLASS_NAME, "no").text
+            tleft = trs[i].find_element(By.CLASS_NAME, "t_left")
+            title = tleft.find_elements(By.TAG_NAME, "a")[1].text
+            artist = trs[i].find_element(By.ID, "artistName").text
+            msl.append(rank)
+            msl.append(title)
+            print(title)
+            msl.append(artist)
+            monthly_top_songs.append(msl)
+        except:
+            print("해당 월에 들은 노래가 없습니다.")
+            print()
+            monthly_top_songs.append("None")
+
+    # trs = tbody.find_elements(By.TAG_NAME, "tr")
+    #
+    # tleft1 = trs[0].find_element(By.CLASS_NAME, "t_left")
+    # title1 = tleft1.find_elements(By.TAG_NAME, "a")[1].text
+    # print(title1)
+    #
+    # for tr in trs:
+    #     msl = []
+    #     rank = tr.find_element(By.CLASS_NAME, "no").text
+    #     tleft = tr.find_element(By.CLASS_NAME, "t_left")
+    #     title = tleft.find_elements(By.TAG_NAME, "a")[1].text
+    #     artist = tr.find_element(By.ID, "artistName").text
+    #     msl.append(rank)
+    #     msl.append(title)
+    #     msl.append(artist)
+    #     monthly_top_songs.append(msl)
+    # print(monthly_top_songs)
+    return monthly_top_songs
 
 # 1년(12달) 클릭 후 크롤링
-def get_one_year():
+def get_one_year(year):
     lists = []
     for i in range(12):
         month_calendar = driver.find_element(By.CLASS_NAME, "month_calendar")
-        month = month_calendar.find_elements(By.CLASS_NAME, "btn") # 12개
-        month[i].click()
+        month_btn = month_calendar.find_elements(By.CLASS_NAME, "btn") # 12개
+        month_btn[i].click()
         print(str(i+1)+"월")
+        month = i+1
         # 선택한 월의 곡 목록 가져오기
-        # get_monthly_song_list()
-        lists.append(get_monthly_song_list())
-        # time.sleep(2)
+        lists.append(get_monthly_top_songs(month, year, 5))
+        # lists.append(get_monthly_1st_song(month, year))
         driver.find_element(By.CLASS_NAME, "d_btn_calenadar").click()
     return lists
 
@@ -64,21 +124,33 @@ def check_year():
     my_list = []
     year = driver.find_element(By.CLASS_NAME, "date").text
     while(True):
-        if year == "2015":
+        if year == "2019":
             print("종료")
             break
+        elif year == "2022":
+        # elif year == "2022" or year == "2021" or year == "2020" or year == "2019":
+            print(year + "::going to previous year")
+            time.sleep(1)
+            driver.find_element(By.CLASS_NAME, "btn_round.small.pre").click()
+            year = driver.find_element(By.CLASS_NAME, "date").text
+            continue
         else:
             print(year)
 
             # 1~12월 곡 리스트 가져오는 함수
-            my_list.append(get_one_year())
+            my_list.append(get_one_year(year))
+            time.sleep(1)
 
             # 이전 년도로 이동
             driver.find_element(By.CLASS_NAME, "btn_round.small.pre").click()
             year = driver.find_element(By.CLASS_NAME, "date").text
 
-    for list in my_list:
-        print(list)
+    # for list in my_list:
+    #     print(list)
+    #     print()
+    #     # save_to_file(list)
+    #     print(type(list))
+    print(my_list)
 
     return True
 
@@ -87,7 +159,7 @@ check_year()
 
 
 
-# 월별보기 클릭 -> 1,2,3,..월 클릭 -> 각 월의 많이들은 리스트 가져오기(get_monthly_song_list) -> 다시 월별보기 클릭 -> 반복
+# 월별보기 클릭 -> 1,2,3,..월 클릭 -> 각 월의 많이들은 리스트 가져오기(get_monthly_top_songs) -> 다시 월별보기 클릭 -> 반복
 
 # lists = []
 # 1월부터 12월 month[0]~month[11] 클릭
@@ -97,8 +169,8 @@ check_year()
 #     month[i].click()
 #     print(str(i+1)+"월")
 #     # 선택한 월의 곡 목록 가져오기
-#     get_monthly_song_list()
-#     # lists.append(get_monthly_song_list())
+#     get_monthly_top_songs()
+#     # lists.append(get_monthly_top_songs())
 #     # time.sleep(2)
 #     driver.find_element(By.CLASS_NAME, "d_btn_calenadar").click()
 #
